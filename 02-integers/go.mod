b91382dd657cfcfa39d4c340b4214750b1755854
evgenymarkov <evgenymarkov@yandex.com>
2023-10-23 23:09:26 +0200

    Solve 02-integers

---
+++ 02-integers/go.mod
@@ -0,0 +1,3 @@
+module github.com/evgenymarkov/learn-golang/02-integers
+
+go 1.21.3
---
+++ 02-integers/integers/adder_test.go
@@ -0,0 +1,21 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
+
+func TestAdder(t *testing.T) {
+	got := Add(2, 2)
+	want := 4
+
+	if got != want {
+		t.Errorf("got '%d' want '%d'", got, want)
+	}
+}
